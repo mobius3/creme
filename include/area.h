668db64b3494e2058336c2377efa371c20ecbecc
Leonardo <leonardo.guilherme@gmail.com>
2019-02-18 16:39:22 -0300

    add area code

---
+++ include/area.h
@@ -0,0 +1,60 @@
+#ifndef CREME_AREA_H
+#define CREME_AREA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "value.h"
+
+/**
+ * Describes an area starting at left, top and ending at right, bottom.
+ *
+ * width, height, center.x and center.y are calculated values (and should be
+ * treated read only!) linked to left, top, right and bottom.
+ *
+ * Note that right and bottom are *not* equivalent to width and height.
+ *
+ * You can (and should!) link these both as down and upstream values of
+ * other values.
+ */
+struct cm_area
+{
+  struct cm_value left;
+  struct cm_value right;
+  struct cm_value top;
+  struct cm_value bottom;
+  struct cm_value width;
+  struct cm_value height;
+  struct
+  {
+    struct cm_value x;
+    struct cm_value y;
+  } center;
+};
+
+void cm_area_construct(struct cm_area * area);
+
+void
+cm_area_construct_set(
+  struct cm_area * area,
+  float left,
+  float top,
+  float right,
+  float bottom);
+
+void
+cm_area_set(
+  struct cm_area * area,
+  float left,
+  float top,
+  float right,
+  float bottom);
+
+void cm_area_destruct(struct cm_area * area);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //CREME_AREA_H
---
+++ src/area.c
@@ -0,0 +1,63 @@
+
+#include <area.h>
+
+#include "area.h"
+
+float cm_area_reduce_subtract(float const values[], uint16_t value_count) {
+  if (value_count < 2) return 0;
+  return values[1] - values[0];
+}
+
+void cm_area_construct(struct cm_area * area) {
+  cm_value_construct(&area->left);
+  cm_value_construct(&area->top);
+  cm_value_construct(&area->right);
+  cm_value_construct(&area->bottom);
+  cm_value_construct_reduce(&area->width, cm_area_reduce_subtract);
+  cm_value_construct_reduce(&area->height, cm_area_reduce_subtract);
+
+  cm_value_construct_average(&area->center.x);
+  cm_value_construct_average(&area->center.y);
+
+  cm_value_link(&area->center.x, &area->left);
+  cm_value_link(&area->center.x, &area->right);
+
+  cm_value_link(&area->center.y, &area->top);
+  cm_value_link(&area->center.y, &area->bottom);
+
+  cm_value_link(&area->width, &area->left);
+  cm_value_link(&area->width, &area->right);
+
+  cm_value_link(&area->height, &area->top);
+  cm_value_link(&area->height, &area->bottom);
+}
+
+void cm_area_destruct(struct cm_area * area) {
+  cm_value_destruct(&area->center.y);
+  cm_value_destruct(&area->center.x);
+  cm_value_destruct(&area->height);
+  cm_value_destruct(&area->width);
+  cm_value_destruct(&area->bottom);
+  cm_value_destruct(&area->right);
+  cm_value_destruct(&area->top);
+  cm_value_destruct(&area->left);
+}
+
+void
+cm_area_construct_set(
+  struct cm_area * area,
+  float left,
+  float top,
+  float right,
+  float bottom) {
+  cm_area_construct(area);
+  cm_area_set(area, left, top, right, bottom);
+}
+
+void
+cm_area_set(struct cm_area * area, float left, float top, float right, float bottom) {
+  cm_value_set(&area->left, left);
+  cm_value_set(&area->top, top);
+  cm_value_set(&area->right, right);
+  cm_value_set(&area->bottom, bottom);
+}

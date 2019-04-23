5b5ccb7b6684eebb716e69314206f75b5af28571
Leonardo <leonardo.guilherme@gmail.com>
2019-04-23 14:09:40 -0300

    update sdl2 rendering example to add four binded frames

--- examples/sdl2-rendering/src/main.c
+++ examples/sdl2-rendering/src/main.c
@@ -11,9 +11,9 @@
 
 
 int main(int argc, const char * argv[]) {
-  SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-
   struct cmex_sdl2_state state;
+
+  SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
 
   cmex_sdl2_state_construct(
     &state,
@@ -27,39 +27,48 @@ int main(int argc, const char * argv[]) {
     .column_count = 80,
     .row_count = 50,
     .frame = {
-      .column = 4,
-      .row = 4
+      .column = 1,
+      .row = 1
     }
   };
 
+
+  /* creates four frames */
+  struct cmw_frame top_left;
+  struct cmw_frame top_right;
+  struct cmw_frame bottom_right;
+  struct cmw_frame bottom_left;
+
+  /* initializes state */
   cmex_sdl2_state_load_tileset(&state, tilesets_kiwi_png.data,
                                tilesets_kiwi_png.length);
 
-  struct cmw_frame frame1;
-  cmw_frame_construct(&frame1);
-  cm_value_link(&frame1.area.right, &state.area.right);
-  cm_value_link(&frame1.area.bottom, &state.area.bottom);
+  /* construct frames */
+  cmw_frame_construct(&top_left);
+  cmw_frame_construct(&top_right);
+  cmw_frame_construct(&bottom_right);
+  cmw_frame_construct(&bottom_left);
 
-  cm_value_offset_set(&frame1.area.left, -100);
-  cm_value_link(&frame1.area.left, &frame1.area.right);
+  /* link frames */
+  cm_value_link(&top_left.area.left, &state.area.left);
+  cm_value_link(&top_left.area.top, &state.area.top);
+  cm_value_link(&top_left.area.right, &state.area.center.x);
+  cm_value_link(&top_left.area.bottom, &state.area.center.y);
 
-  cm_value_offset_set(&frame1.area.top, -100);
-  cm_value_link(&frame1.area.top, &frame1.area.bottom);
+  cm_value_link(&top_right.area.left, &top_left.area.right);
+  cm_value_link(&top_right.area.top, &state.area.top);
+  cm_value_link(&top_right.area.right, &state.area.right);
+  cm_value_link(&top_right.area.bottom, &state.area.center.y);
 
-  struct cmw_frame frame2;
-  cmw_frame_construct(&frame2);
+  cm_value_link(&bottom_right.area.left, &state.area.center.x);
+  cm_value_link(&bottom_right.area.top, &top_left.area.bottom);
+  cm_value_link(&bottom_right.area.right, &state.area.right);
+  cm_value_link(&bottom_right.area.bottom, &state.area.bottom);
 
-  cm_value_offset_set(&frame2.area.right, 50);
-  cm_value_link(&frame2.area.right, &frame1.area.left);
-
-  cm_value_offset_set(&frame2.area.bottom, 50);
-  cm_value_link(&frame2.area.bottom, &frame1.area.top);
-
-  cm_value_offset_set(&frame2.area.top, -100);
-  cm_value_link(&frame2.area.top, &frame2.area.bottom);
-
-/*  cm_value_offset_set(&frame2.area.left, -100); */
-  cm_value_link(&frame2.area.left, &state.area.left);
+  cm_value_link(&bottom_left.area.left, &state.area.left);
+  cm_value_link(&bottom_left.area.top, &state.area.center.y);
+  cm_value_link(&bottom_left.area.right, &bottom_right.area.left);
+  cm_value_link(&bottom_left.area.bottom, &state.area.bottom);
 
   struct cm_render_command buffer[256];
   struct cm_render_queue queue;
@@ -70,8 +79,11 @@ int main(int argc, const char * argv[]) {
 
     cmex_sdl2_state_update(&state);
     cm_render_queue_flush(&queue);
-    cmw_frame_render(&frame1, &tileset, &queue);
-    cmw_frame_render(&frame2, &tileset, &queue);
+
+    cmw_frame_render(&top_left, &tileset, &queue);
+    cmw_frame_render(&top_right, &tileset, &queue);
+    cmw_frame_render(&bottom_right, &tileset, &queue);
+    cmw_frame_render(&bottom_left, &tileset, &queue);
 
     SDL_RenderClear(state.sdl_renderer);
 

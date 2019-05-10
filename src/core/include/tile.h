c3b83244c2ff2c98e66ac673a61a1abe007cd8c8
Leonardo <leonardo.guilherme@gmail.com>
2019-05-10 01:51:08 -0300

    fix cs in tile.h

--- src/core/include/tile.h
+++ src/core/include/tile.h
@@ -35,7 +35,8 @@ CORE_API extern struct cm_tile cm_tile_make(uint16_t column, uint16_t row);
  * @param column The column of this tile
  * @param row The row of this tile
  */
-CORE_API extern void cm_tile_set(struct cm_tile * tile, uint16_t column, uint16_t row);
+CORE_API extern void
+cm_tile_set(struct cm_tile * tile, uint16_t column, uint16_t row);
 
 /**
  * Creates and returns a `cm_tile` value that is at the
@@ -53,7 +54,8 @@ CORE_API extern struct cm_tile cm_tile_left_of(struct cm_tile const * center);
  * @param center A pointer to a `cm_tile` value representing the center
  * @return A `cm_tile` value with row and column top-left relative to center
  */
-CORE_API extern struct cm_tile cm_tile_top_left_of(struct cm_tile const * center);
+CORE_API extern struct cm_tile
+cm_tile_top_left_of(struct cm_tile const * center);
 
 /**
  * Creates and returns a `cm_tile` value that is at the
@@ -71,7 +73,8 @@ CORE_API extern struct cm_tile cm_tile_top_of(struct cm_tile const * center);
  * @param center A pointer to a `cm_tile` value representing the center
  * @return A `cm_tile` value with row and column top-right relative to center
  */
-CORE_API extern struct cm_tile cm_tile_top_right_of(struct cm_tile const * center);
+CORE_API extern struct cm_tile
+cm_tile_top_right_of(struct cm_tile const * center);
 
 /**
  * Creates and returns a `cm_tile` value that is at the
@@ -89,7 +92,8 @@ CORE_API extern struct cm_tile cm_tile_right_of(struct cm_tile const * center);
  * @param center A pointer to a `cm_tile` value representing the center
  * @return A `cm_tile` value with row and column bottom-right relative to center
  */
-CORE_API extern struct cm_tile cm_tile_bottom_right_of(struct cm_tile const * center);
+CORE_API extern struct cm_tile
+cm_tile_bottom_right_of(struct cm_tile const * center);
 
 /**
  * Creates and returns a `cm_tile` value that is at the
@@ -107,7 +111,8 @@ CORE_API extern struct cm_tile cm_tile_bottom_of(struct cm_tile const * center);
  * @param center A pointer to a `cm_tile` value representing the center
  * @return A `cm_tile` value with row and column bottom-left relative to center
  */
-CORE_API extern struct cm_tile cm_tile_bottom_left_of(struct cm_tile const * center);
+CORE_API extern struct cm_tile
+cm_tile_bottom_left_of(struct cm_tile const * center);
 
 #ifdef __cplusplus
 }
